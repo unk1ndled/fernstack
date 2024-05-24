a45b4ee6d49646a738171bc90e9ac0be2a0d5e2c
Haithaaam21 <oumanhou.haitham0123@gmail.com>
2024-05-24 19:16:31 +0100

    Show File + Copy Link + Rename File

--- src/componenets/Folder.js
+++ src/componenets/Folder.js
@@ -10,6 +10,7 @@ import {
   CardBody,
   CardHeader,
   Image,
+  useDisclosure,
 } from "@nextui-org/react";
 
 import { deleteDoc, getDocs, query, where } from "firebase/firestore";
@@ -21,10 +22,13 @@ import { sleep } from "../functions/sleep";
 import { deleteObject, ref } from "firebase/storage";
 import { updateStorage } from "../functions/updatestorage";
 import { useAuth } from "../contexts/AuthContext";
+import RenameModal from "./RenameModal";
 
 const Folder = ({ folder, update }) => {
   const navigate = useNavigate();
   const { currentUser } = useAuth();
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const openFolder = () => {
     navigate("/treasure/" + folder.id, { state: { folder: folder } });
@@ -61,11 +65,11 @@ const Folder = ({ folder, update }) => {
     return freedstorage;
   };
 
-  const shareFolder = () => {
-    const link = `http://localhost:3000/treasure/${folder.id}`;
-    navigator.clipboard.writeText(link);
-    alert("Link copied to clipboard: ");
-  };
+  // const shareFolder = () => {
+  //   const link = `http://localhost:3000/treasure/${folder.id}`;
+  //   navigator.clipboard.writeText(link);
+  //   alert("Link copied to clipboard: ");
+  // };
 
   const delDoc = async () => {
     try {
@@ -78,63 +82,87 @@ const Folder = ({ folder, update }) => {
     }
   };
 
+  const openModal = () => {
+    onOpen();
+  };
+
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Card shadow="sm" isPressable>
-          <CardHeader className="text-small flex items-center justify-center ">
-            <b className="max-w-24 truncate ">{folder.name}</b>
-            <p className="text-default-500">{folder.size}</p>
-          </CardHeader>
-          <CardBody className="overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={folder.name}
-              className="w-full object-cover h-[140px]"
-              src={FolderIcon}
-            />
-          </CardBody>
-        </Card>
-      </DropdownTrigger>
-      <DropdownMenu variant="faded" aria-label="Dropdown menu with description">
-        <DropdownSection title="Actions" showDivider>
-          <DropdownItem
-            key="open"
-            description="Check folder contents"
-            onPress={() => {
-              openFolder();
-            }}
-          >
-            Open
-          </DropdownItem>
-          <DropdownItem key="download">Download</DropdownItem>
-          <DropdownItem
-            key="copy"
-            description="Copy the Folder link"
-            onPress={() => {
-              shareFolder();
-            }}
-          >
-            Copy link
-          </DropdownItem>
-        </DropdownSection>
-        <DropdownSection title="Danger zone">
-          <DropdownItem
-            key="delete"
-            className="text-danger"
-            color="danger"
-            description="Permanently delete the Fodler"
-            onPress={() => {
-              delDoc();
-            }}
-          >
-            Delete folder
-          </DropdownItem>
-        </DropdownSection>
-      </DropdownMenu>
-    </Dropdown>
+    <>
+      <RenameModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        folder={folder}
+      ></RenameModal>
+      <Dropdown>
+        <DropdownTrigger>
+          <Card shadow="sm" isPressable>
+            <CardHeader className="text-small flex items-center justify-center ">
+              <b className="max-w-24 truncate ">{folder.name}</b>
+              <p className="text-default-500">{folder.size}</p>
+            </CardHeader>
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={folder.name}
+                className="w-full object-cover h-[140px]"
+                src={FolderIcon}
+              />
+            </CardBody>
+          </Card>
+        </DropdownTrigger>
+        <DropdownMenu
+          variant="faded"
+          aria-label="Dropdown menu with description"
+        >
+          <DropdownSection title="Actions" showDivider>
+            <DropdownItem
+              key="open"
+              description="Check folder contents"
+              onPress={() => {
+                openFolder();
+              }}
+            >
+              Open
+            </DropdownItem>
+
+            <DropdownItem
+              key="Rename"
+              description="Rename your Folder"
+              onPress={() => {
+                onOpen();
+              }}
+            >
+              Rename
+            </DropdownItem>
+
+            {/* <DropdownItem
+  key="copy"
+  description="Copy the Folder link"
+  onPress={() => {
+    shareFolder();
+  }}
+>
+  Copy link
+</DropdownItem> */}
+          </DropdownSection>
+          <DropdownSection title="Danger zone">
+            <DropdownItem
+              key="delete"
+              className="text-danger"
+              color="danger"
+              description="Permanently delete the Fodler"
+              onPress={() => {
+                delDoc();
+              }}
+            >
+              Delete folder
+            </DropdownItem>
+          </DropdownSection>
+        </DropdownMenu>
+      </Dropdown>
+    </>
   );
 };
 
